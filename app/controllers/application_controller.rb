8bdc215fba177f7c304fe13814f0dea629a26ced
Tinco Andringa <mail@tinco.nl>
2018-11-05 10:17:00 +0100

    Initial rails app with flats

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :do_a_dance
+
+  def do_a_dance
+    Rails.logger.info "Doing a dance."
+  end
 end
